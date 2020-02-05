49eb0604f7ca5573692a310e2a2f6ad70f6895fc
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-02-05 20:05:55 +0000

    quick sort algorithm (recursive)

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,43 +1,47 @@
 #include "sort.h"
+#include "stdio.h"
 /**
-  *
-  *
-  *
+  *partition - partitions the array
+  *@array: array to partition
+  *@start: start index of the array
+  *@end: end index of the array
+  *@size: size of the array
+  *Return: index
   */
-size_t partition(int *array, size_t start,size_t end, size_t size)
+int partition(int *array, int start, int end, size_t size)
 {
-	size_t index, i;
-	int pivot, temp, temp2;
+	int pivot, i, temp, temp2, index;
 
 	pivot = array[end];
 	index = start;
-	for (i = start; i < end; ++i)
+	for (i = start; i < end; i++)
 	{
-		if (array[i] <= pivot)
+		if (array[i] < pivot)
 		{
-			//swap(array[i], array[index]);
 			temp = array[i];
 			array[i] = array[index];
 			array[index] = temp;
 			index = index + 1;
-			print_array(array, size);
+			if (index < i)
+				print_array(array, size);
 		}
 	}
-	//swap(array[index], array[end]);
 	temp2 = array[index];
 	array[index] = array[end];
 	array[end] = temp2;
 	print_array(array, size);
-	return index;
+	return (index);
 }
 /**
-  *quick_sort_mimicry
-  *
-  *
+  *quick_sort_mimicry - sorts using quick sorts algorithm
+  *@array: array to sort
+  *@start: start index of the array
+  *@end: end index of the array
+  *size: size of the array
   */
-void quick_sort_mimicry(int *array, size_t start, size_t end, size_t size)
+void quick_sort_mimicry(int *array, int start, int end, size_t size)
 {
-	size_t index;
+	int index;
 
 	if (start < end)
 	{
@@ -47,11 +51,12 @@ void quick_sort_mimicry(int *array, size_t start, size_t end, size_t size)
 	}
 }
 /**
-  *
-  *
-  *
+  *quick_sort - function to sort using quick sort
+  *@array: array to sort
+  *@size: size of array
   */
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_mimicry(array, 0, size - 1, size);
+	if (size > 1)
+		quick_sort_mimicry(array, 0, size - 1, size);
 }
